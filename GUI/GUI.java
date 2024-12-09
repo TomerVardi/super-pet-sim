fcc9bc62a0098337df38e57ec8e056b9ed34c76f
TomerVardi <89004132+TomerVardi@users.noreply.github.com>
2024-12-09 09:55:35 -0800

    Update GUI.java

--- GUI/GUI.java
+++ GUI/GUI.java
@@ -1,1 +1,15 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
 
+public class GUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Super Pet Simulator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        panel.setBackground(java.awt.Color.WHITE);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+}
