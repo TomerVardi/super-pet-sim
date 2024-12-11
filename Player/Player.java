0d0a64f2f857238ece02c7bfeba1ee5c26911a6f
NeoChang-P4 <556415@my.cuhsd.org>
2024-12-11 09:51:06 -0800

    Update Player.java
    added constructor

--- Player/Player.java
+++ Player/Player.java
@@ -1,4 +1,16 @@
 Public Class Player{
   //name of the player
  private String name; 
+ // constructor of the player
+  Public Player(String name){
+    this.name=name;
+}
+// @return name 
+Public String getName(){
+    return name;
+  }
+// sets name
+  Public void setName(){
+    this.name = name;
+}
 }
