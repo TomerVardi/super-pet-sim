6c2570e26e8fcacff2b316b3998b4c35e97378a5
NeoChang-P4 <556415@my.cuhsd.org>
2024-12-11 23:38:29 -0800

    Update Pet.java
    added iteration (feeding pet and some instance variables for them)

--- Pet/Pet.java
+++ Pet/Pet.java
@@ -5,14 +5,34 @@ public class Pet {
     private boolean petType;
     // Age
     private final int age;
-
+    // Hunger level from 0 (not hungry) to 10 (very hungry)
+    private int hungerLevel; 
+    // Happiness level from 0 (unhappy) to 10 (very happy)
+    private int happiness;    
+    
     public Pet(String name, boolean petType, int age) {
         this.name = name;
         this.petType = petType;
         this.age = age;
+        // Default hunger level (neutral state)
+        this.hungerLevel = 5;   
+        // Default happiness level (neutral state)
+        this.happiness = 5;     
+
     }
 
-
+    public void feed(int foodAmount) {
+    System.out.println(name + " is being fed!");
+    for (int i = 0; i < foodAmount; i++) {
+            // Each feeding decreases hunger level
+            if (hungerLevel > 0) {
+                hungerLevel--;
+                System.out.println(name + " is eating... Hunger level: " + hungerLevel);
+            } else {
+                System.out.println(name + " is not hungry anymore.");
+                break;  // If hunger is satisfied, stop feeding
+            }
+    }
     // returns name
     public String getName() {
         return name;
